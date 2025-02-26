3f625b5536f79a54e9af6d272c0ed64fa930417e
juanjouni <jj.villanueva.2022@alumnos.urjc.es>
2025-02-26 20:26:30 +0100

    Update of folders

--- practica1/src/main/java/es/codeurjc/practica1/Application.java
+++ practica1/src/main/java/es/codeurjc/practica1/Application.java
@@ -1,10 +1,10 @@
-package es.codeurjc.demo;
+package es.codeurjc.practica1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
