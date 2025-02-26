983a373325810cf19539bc65f7ea3eb769700930
juanjouni <jj.villanueva.2022@alumnos.urjc.es>
2025-02-26 17:43:53 +0100

    first commit

---
+++ demo/src/test/java/es/codeurjc/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package es.codeurjc.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
