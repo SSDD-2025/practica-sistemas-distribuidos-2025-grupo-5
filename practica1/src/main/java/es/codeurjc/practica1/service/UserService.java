86d4dffe694a60c2ccf11cadcf24023605f0689c
paulaM04 <pm.barroso.2022@alumnos.urjc.es>
2025-02-27 10:39:57 +0100

    clase6: prueba1

--- practica1/src/main/java/es/codeurjc/practica1/service/UserService.java
+++ practica1/src/main/java/es/codeurjc/practica1/service/UserService.java
@@ -4,10 +4,12 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import es.codeurjc.practica1.model.User;
 import es.codeurjc.practica1.repositories.UserRepository;
 
+@Service
 public class UserService {
 
     @Autowired
