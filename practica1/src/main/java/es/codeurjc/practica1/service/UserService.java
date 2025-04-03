ccab554a958e3876c820e87ee25dc81e40dcc858
dvcoronado <d.coronado.2022@alumnos.urjc.es>
2025-04-03 12:02:01 +0200

    testting solutions

--- practica1/src/main/java/es/codeurjc/practica1/service/UserService.java
+++ practica1/src/main/java/es/codeurjc/practica1/service/UserService.java
@@ -1,15 +1,9 @@
 package es.codeurjc.practica1.service;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import es.codeurjc.practica1.model.Order;
@@ -17,25 +11,9 @@ import es.codeurjc.practica1.model.User;
 import es.codeurjc.practica1.repositories.UserRepository;
 
 @Service
-public class UserService implements UserDetailsService {
+public class UserService {
     @Autowired
     private UserRepository userRepository;
-
-    
-    @Override
-    public UserDetails loadUserByUsername(String username)
-    throws UsernameNotFoundException {
-        User user = userRepository.findByName(username)
-            .orElseThrow(() -> new UsernameNotFoundException("User not found"));
-
-        List<GrantedAuthority> roles = new ArrayList<>();
-
-        for (String role : user.getRoles()) {
-            roles.add(new SimpleGrantedAuthority("ROLE_" + role));
-        }
-            return new org.springframework.security.core.userdetails.User(user.getName(),
-        user.getPassword(), roles);
-    }
 
     public List<User> findAll() {
         return userRepository.findAll();
