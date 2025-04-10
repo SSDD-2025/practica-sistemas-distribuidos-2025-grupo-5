62680223058c5ba1e12fd8d070ff59aeccc798b0
paulaM04 <pm.barroso.2022@alumnos.urjc.es>
2025-04-10 13:02:14 +0200

    Co-authored-by: dvcoronado <dvcoronado@users.noreply.github.com>

--- practica1/src/main/java/es/codeurjc/practica1/controller/ProductController.java
+++ practica1/src/main/java/es/codeurjc/practica1/controller/ProductController.java
@@ -23,14 +23,20 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.server.ResponseStatusException;
 
+import es.codeurjc.practica1.dto.ProductDTO;
 import es.codeurjc.practica1.model.Order;
 import es.codeurjc.practica1.model.Product;
 import es.codeurjc.practica1.model.Review;
@@ -60,6 +66,91 @@ public class ProductController {
 	@Autowired
 	private ReviewService reviewService;
 
+@RestController
+@RequestMapping("/api/Products")
+public class ProductRestController {
+
+	@Autowired
+	private ProductService productService;
+
+	@GetMapping("/")
+	public List<ProductDTO> getProducts() {
+		return productService.findProducts();
+	}
+
+	@GetMapping("/{id}")
+	public ProductDTO getProduct(@PathVariable long id) {
+
+		return productService.getProduct(id);
+	}
+
+	@PostMapping("/")
+	public ResponseEntity<ProductDTO> createProduct(@RequestBody ProductDTO ProductDTO) {
+
+		ProductDTO = productService.save(ProductDTO);
+
+		URI location = fromCurrentRequest().path("/{id}").buildAndExpand(ProductDTO.id()).toUri();
+
+		return ResponseEntity.created(location).body(ProductDTO);
+	}
+
+	@PutMapping("/{id}")
+	public ProductDTO replaceProduct(@PathVariable long id, @RequestBody ProductDTO updatedProductDTO) throws SQLException {
+
+		return productService.replaceProduct(id, updatedProductDTO);
+	}
+
+	@DeleteMapping("/{id}")
+	public ProductDTO deleteProduct(@PathVariable long id) {
+
+		return productService.deleteProduct(id);
+	}
+
+	@PostMapping("/{id}/image")
+	public ResponseEntity<Object> createProductImage(@PathVariable long id, @RequestParam MultipartFile imageFile)
+			throws IOException {
+
+		productService.createProductImage(id, imageFile.getInputStream(), imageFile.getSize());
+
+		URI location = fromCurrentRequest().build().toUri();
+
+		return ResponseEntity.created(location).build();
+	}
+
+	@GetMapping("/{id}/image")
+	public ResponseEntity<Object> getProductImage(@PathVariable long id) throws SQLException, IOException {
+
+		Resource postImage = productService.getProductImage(id);
+
+		return ResponseEntity
+				.ok()
+				.header(HttpHeaders.CONTENT_TYPE, "image/jpeg")
+				.body(postImage);
+
+	}
+
+	@PutMapping("/{id}/image")
+	public ResponseEntity<Object> replaceProductImage(@PathVariable long id, @RequestParam MultipartFile imageFile)
+			throws IOException {
+
+		productService.replaceProductImage(id, imageFile.getInputStream(), imageFile.getSize());
+
+		return ResponseEntity.noContent().build();
+	}
+
+	@DeleteMapping("/{id}/image")
+	public ResponseEntity<Object> deleteProductImage(@PathVariable long id) throws IOException {
+
+		productService.deleteProductImage(id);
+
+		return ResponseEntity.noContent().build();
+	}
+}
+
+
+
+
+/*--------------------------------------------------------------------------------------------------------------------
 	@GetMapping("/")
 	public String showProducts(Model model) {
 		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
@@ -730,6 +821,8 @@ public class ProductController {
 		}
 	}
 
+
+*/
 }
 	
 
