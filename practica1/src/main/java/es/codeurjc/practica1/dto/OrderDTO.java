e3318425f9608ccff18473f655545d8cdd131d3f
juanjouni <jj.villanueva.2022@alumnos.urjc.es>
2025-04-14 23:12:26 +0200

    update prueba

--- practica1/src/main/java/es/codeurjc/practica1/dto/OrderDTO.java
+++
@@ -1,9 +0,0 @@
-package es.codeurjc.practica1.dto;
-
-import java.util.List;
-
-public record OrderDTO (
-    Long id,
-    Double totalPrice,
-    UserDTO owner,
-    List<ProductDTO> products){}
--- practica1/src/main/java/es/codeurjc/practica1/repositories/ReviewRepository.java
+++
@@ -1,9 +0,0 @@
-package es.codeurjc.practica1.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import es.codeurjc.practica1.model.Review;
-
-public interface ReviewRepository extends JpaRepository<Review, Long> {
-
-}
